cb8c4c67a1457ed2962b70b48bb582698e57e9d8
KirSaxhlil <dimapisarenko28@gmail.com>
2023-02-11 06:31:33 +0300

    lab 1 ex2-2

--- lab1/ex2.rb
+++ lab1/ex2.rb
@@ -1,7 +1,8 @@
 def divider_quantity_not_3(number)
+	temp_number = number.abs
 	quantity = 0
-	2.upto(number-1) do |divider|
-		if 0 == number % divider
+	2.upto(temp_number-1) do |divider|
+		if 0 == temp_number % divider
 			if 0 != divider % 3
 				quantity += 1
 			end
@@ -10,4 +11,25 @@ def divider_quantity_not_3(number)
 	return quantity
 end
 
-puts "#{divider_quantity_not_3(15)}"
+puts "#{divider_quantity_not_3(-15)}"
+
+def minimal_odd_digit(number)
+	min = 11
+	temp_number = number.abs
+	until 0 == temp_number
+		digit = temp_number % 10
+		if 1 == digit % 2
+			if min > digit
+				min = digit
+			end
+		end
+		temp_number /= 10
+	end
+	if 11 == min
+		return -1
+	else
+		return min
+	end
+end
+
+puts "#{minimal_odd_digit(-84862)}"

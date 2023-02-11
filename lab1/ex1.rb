905e1a5abbfb580694826f7375378cb18ce1260d
KirSaxhlil <dimapisarenko28@gmail.com>
2023-02-11 05:03:31 +0300

    lab1 ex1-2

--- lab1/ex1.rb
+++ lab1/ex1.rb
@@ -1,1 +1,23 @@
 puts "Hello, World!"
+
+puts "Hello, #{ARGV[0]}"
+
+puts "What your favourite language?"
+lang = STDIN.gets.chomp.downcase
+
+case lang
+	when "ruby"
+		puts "padleza"
+	else
+		puts "Skora bdet rube."
+		case lang
+			when "c++"
+				puts "Respeckckckt"
+			when "java"
+				puts "srinje"
+			when "python"
+				puts "I am antipythoner, leave."
+			else
+				puts "idk what is it"
+		end
+end

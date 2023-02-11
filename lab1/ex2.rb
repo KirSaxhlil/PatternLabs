cf932ce016fb3decc8ae8aed55db4afeb6a846fa
KirSaxhlil <dimapisarenko28@gmail.com>
2023-02-11 10:00:19 +0300

    lab1 ex2-3

--- lab1/ex2.rb
+++ lab1/ex2.rb
@@ -33,3 +33,55 @@ def minimal_odd_digit(number)
 end
 
 puts "#{minimal_odd_digit(-84862)}"
+
+def digit_sum(number)
+	sum = 0
+	temp_number = number.abs
+	begin
+		sum += temp_number % 10
+		temp_number /= 10
+	end while temp_number !=0
+	return sum
+end
+
+def digit_multiply(number)
+	sum = 1
+	temp_number = number.abs
+	begin
+		sum *= temp_number % 10
+		temp_number /= 10
+	end while temp_number !=0
+	return sum
+end
+
+def GCD(number1, number2)
+	temp1 = number1.abs
+	temp2 = number2.abs
+	
+	while temp1 != temp2
+		if temp1 < temp2
+			transfer = temp1
+			temp1 = temp2
+			temp2 = transfer
+		end
+		temp1 -= temp2
+	end
+	return temp1
+end
+
+def divider_sum_spec(number)
+	temp_number = number.abs
+	sum = 0
+	number_digit_sum = digit_sum(number)
+	number_digit_multiply = digit_multiply(number)
+	2.upto(temp_number-1) do |divider|
+		if 0 == temp_number % divider
+			if 1 == GCD(divider, number_digit_sum) and 1 != GCD(divider, number_digit_multiply)
+				sum += divider
+			end
+		end
+	end
+	return sum
+end
+
+puts "#{divider_sum_spec(14)}"

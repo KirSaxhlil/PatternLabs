f0b455e9c8f19df961682fcb4a9f9be818dd169b
KirSaxhlil <dimapisarenko28@gmail.com>
2023-03-03 18:02:41 +0300

    Lab1 ex3-1 MaxFunc

--- lab1/ex3.rb
+++ lab1/ex3.rb
@@ -4,19 +4,43 @@ a = []
 
 def find_min_in_array(array)
 	if array.empty? #if array has no elements, it has no min
-		return -1
+		return nil
 	end
 	min = array[0]
 	for number in array
 		if nil == number # check for bad values
-			return -1
+			return nil
 		end
-		if number < min
-			min = number
+		begin
+			if number < min
+				min = number
+			end
+		rescue ArgumentError  #check for non one-type array
+			return nil
 		end
 	end
 	return min
 end
 
-b = find_min_in_array(a)
+def find_max_in_array(array)
+	if array.empty? #if array has no elements, it has no max
+		return nil
+	end
+	max = array[0]
+	for number in array
+		if nil == number # check for bad values
+			return nil
+		end
+		begin
+			if number > max
+				max = number
+			end
+		rescue ArgumentError #check for non one-type array
+			return nil
+		end
+	end
+	return max
+end
+
+b = find_max_in_array(a)
 puts b

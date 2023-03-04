ed2b27a6d561aef1f4bd2e4962a7282e2bd626bc
KirSaxhlil <dimapisarenko28@gmail.com>
2023-03-04 07:07:24 +0300

    Lab1 ex4-2

--- lab1/ex4.rb
+++ lab1/ex4.rb
@@ -1,16 +1,32 @@
 def find_one_different(array)
-	if array.empty?
+	if array.empty? #we can't find anything in empty array
 		return -1
 	end
 	test = array[0]
 	array.delete(test)
-	if array.length == 1
+	if array.length == 1 #if one last, this last is different, in another way was deleted only one item< which is different
 		return array[0]
 	else
 		return test
 	end
 end
 
-array = [2,1,1,1,1,1,1,1]
+def find_two_mins(array)
+	if array.empty? #we can't find anything in empty array
+		return -1
+	end
+	mins = []
+	mins << array.min
+	array.delete(mins[0])
+	if array.empty? #for those ways, when original array collected only one item, or many items, but they all was same
+		return mins
+	end
+	mins << array.min
+	return mins
+end
 
-puts find_one_different(array)
+array1 = [2,1,1,1,1,1,1,1]
+array2 = [3,4,4,3,5,1,1,2,2,3,4]
+
+#puts find_one_different(array1)
+puts find_two_mins(array2)

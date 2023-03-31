b7c00883ca64238ed843329357971859fc083d3c
KirSaxhlil <dimapisarenko28@gmail.com>
2023-03-31 08:39:43 +0300

    Lab2 ex2-1 Constructor refactoring
    Now constructor can get hash or string and call required method for process it.
    
    Also, when setters gets empty string, its uses it for clearing field.

--- lab2/main.rb
+++ lab2/main.rb
@@ -1,16 +1,19 @@
 require_relative "student.rb"
 
-object1 = Student.new({name:"Arbux", family:"Alebarod", patronymic:"Achekavich", phone_number:"+79189356731", email:"address@mail.sd", telegram:"@ahto_ahaha", git:"https://github.com/CyBeR_uSeR"})
-object2 = Student.new({name:"Ti", family: "Prikki", patronymic:"Noitaich", email:"my_bestEmail3@cybermail.gog", git:"gitlab.com/stellAr"})
-object3 = Student.new({name:"lARrius", family:"VarrO", patronymic:"sToRyViCh", phone_number:79336421496, telegram:"BloodBath"})
+object1 = Student.new(hash:{name:"Arbux", family:"Alebarod", patronymic:"Achekavich", phone_number:"+79189356731", email:"address@mail.sd", telegram:"@ahto_ahaha", git:"https://github.com/CyBeR_uSeR"})
+#object2 = Student.new(hash:{name:"Ti", family: "Prikki", patronymic:"Noitaich", email:"my_bestEmail3@cybermail.gog", git:"gitlab.com/stellAr"})
+#object3 = Student.new(hash:{name:"lARrius", family:"VarrO", patronymic:"sToRyViCh", phone_number:79336421496, telegram:"BloodBath"})
+object4 = Student.new(string:"NAME;FAMILIAR;PAPAPAPA;89882883838;;gigaChad;")
+object5 = Student.new()
 
 puts object1.get_info()
-puts object2.get_info()
-puts object3.get_info()
+#puts object2.get_info()
+#puts object3.get_info()
+puts object4.get_info()
 
-object3.set_contacts({email:"aba@sringe.com", telegram:""})
-puts object3.get_info()
+#object3.set_contacts({email:"aba@sringe.com", telegram:nil})
+#puts object3.get_info()
 
-puts object1.validate()
-puts object2.validate()
-puts object3.validate()
+#puts object1.validate()
+#puts object2.validate()
+#puts object3.validate()
--- lab2/student.rb
+++ lab2/student.rb
@@ -1,19 +1,36 @@
 class Student
 	attr_reader :name, :family, :patronymic, :phone_number, :email, :telegram, :git
 	
-	def initialize(hash)
+	def initialize(hash:nil, string:nil)
+		if hash != nil
+			constructor_hash(hash)
+		elsif string != nil
+			constructor_string(string)
+		else
+			constructor_default()
+		end
+	end
+	
+	private
+	
+	def constructor_default()
+		raise ArgumentError.new "Student constructor needs data."
+	end
+	def constructor_hash(hash)
 		#name, family and patronymic are necessary, others optional
 		if(hash[:name] == nil or hash[:family] == nil or hash[:patronymic] == nil)
 			raise ArgumentError.new "Name, family and patronymic are necessary for constructor."
 		end
-		def_hash = hash
-		def_hash.default = ""
-		set_name(new_name:def_hash[:name], new_family:def_hash[:family], new_patronymic:def_hash[:patronymic])
-		self.phone_number = def_hash[:phone_number]
-		self.email = def_hash[:email]
-		self.telegram = def_hash[:telegram]
-		self.git = def_hash[:git]
+		set_name({name:hash[:name], family:hash[:family], patronymic:hash[:patronymic]})
+		set_contacts({phone_number:hash[:phone_number], email:hash[:email], telegram:hash[:telegram]})
+		self.git = hash[:git]
 	end
+	def constructor_string(string)
+		splitted = string.split(';')
+		constructor_hash({name:splitted[0], family:splitted[1], patronymic:splitted[2], phone_number:splitted[3], email:splitted[4], telegram:splitted[5], git:splitted[6]})
+	end
+	
+	public
 	
 	def set_name(hash)
 		if hash[:name] != nil #if we not input name, not change this
@@ -30,7 +47,7 @@ class Student
 				raise ArgumentError.new "Wrong family."
 			end
 		end
-		if hash[:patronymic] != "" #if we not input patronymic, not change this
+		if hash[:patronymic] != nil #if we not input patronymic, not change this
 			if hash[:patronymic].match(/^[a-zA-Z]+$/)
 				@patronymic = hash[:patronymic].capitalize
 			else
@@ -41,25 +58,25 @@ class Student
 	
 	def phone_number=(new_number)
 		if new_number.class == String
-			if new_number == ""
-				@phone_number = ""
-				return
-			end
-			if Student.is_phone_number(new_number)
+			if new_number.empty?
+				self.phone_number = nil
+			elsif Student.is_phone_number(new_number)
 				@phone_number = new_number
 			else
 				raise ArgumentError.new "#{new_number} is not a phone number."
 			end
 		elsif new_number.class == Integer
 			self.phone_number = (new_number.to_s) #we are integers haters
+		elsif new_number == nil
+			@phone_number = nil
 		else
 			raise TypeError.new "Phone number must be String or Integer."
 		end
 	end
 	
 	def email=(new_email)
-		if new_email == "" # if we set empty string, then we clear email
-			@email = ""
+		if new_email == nil or new_email == "" # if we set nil, then we clear email
+			@email = nil
 		elsif Student.is_email(new_email)
 			@email = new_email
 		else
@@ -69,9 +86,8 @@ class Student
 	
 	def telegram=(new_telegram)
 		if new_telegram.class == String
-			if new_telegram == ""
-				@telegram = "" #clearing telegram field
-				return
+			if new_telegram.empty?
+				self.telegram = nil
 			end
 			dog = false
 			if new_telegram[0] == '@' #check for first staying @
@@ -82,14 +98,16 @@ class Student
 			else
 				raise ArgumentError.new  "Wrong telegram user id."
 			end
+		elsif new_telegram == nil
+			@telegram = nil
 		else
 			raise TypeError.new "Telegram must be String value."
 		end
 	end
 	
 	def git=(new_git)
-		if new_git == "" # if we set empty string, then we clear git
-			@git = ""
+		if new_git == nil or new_git == "" # if we set nil, then we clear git
+			@git = nil
 		elsif Student.is_git(new_git)
 			@git = new_git
 		else
@@ -100,10 +118,10 @@ class Student
 	def get_info
 		info = ""
 		info += "This is info about: #{self.family} #{self.name} #{self.patronymic}" + "\n"
-		info += "Phone number: #{self.phone_number}" + "\n" if self.phone_number != ""
-		info += "Email: #{self.email}" + "\n" if self.email != ""
-		info += "Telegram: #{self.telegram}" + "\n" if self.telegram != ""
-		info += "Git: #{self.git}" + "\n" if self.git != ""
+		info += "Phone number: #{self.phone_number}" + "\n" if self.phone_number != nil
+		info += "Email: #{self.email}" + "\n" if self.email != nil
+		info += "Telegram: #{self.telegram}" + "\n" if self.telegram != nil
+		info += "Git: #{self.git}" + "\n" if self.git != nil
 		return info
 	end
 	
@@ -140,19 +158,19 @@ class Student
 	end
 	
 	def has_git()
-		return git != ""
+		return git != nil
 	end
 	
 	def has_phone_number()
-		return phone_number != ""
+		return phone_number != nil
 	end
 	
 	def has_email()
-		return email != ""
+		return email != nil
 	end
 	
 	def has_telegram()
-		return telegram != ""
+		return telegram != nil
 	end
 	
 	def has_contact()
@@ -164,10 +182,9 @@ class Student
 	end
 	
 	def set_contacts(contacts)
-		temp_contacts = contacts
-		self.email = temp_contacts[:email] if(temp_contacts[:email] != nil)
-		self.phone_number = temp_contacts[:phone_number] if(temp_contacts[:phone_number] != nil)
-		self.telegram = temp_contacts[:telegram] if(temp_contacts[:telegram] != nil)
+		self.email = contacts[:email] if(contacts[:email] != nil)
+		self.phone_number = contacts[:phone_number] if(contacts[:phone_number] != nil)
+		self.telegram = contacts[:telegram] if(contacts[:telegram] != nil)
 	end
 	
 end

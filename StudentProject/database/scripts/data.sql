edff8784713d3beabe57d80650a86951cb5c2131
KirSaxhlil <dimapisarenko28@gmail.com>
2023-04-14 13:07:10 +0300

    Лаб.3 Здч.1-2 Написаны скрипты базы

---
+++ StudentProject/database/scripts/data.sql
@@ -0,0 +1,11 @@
+Insert into Students (Name, Family, Patronymic, Git, PhoneNumber, Telegram, Email)
+Values ("Григорий", "Григорьев", "Григорьевич", "github.com/zaza", null, "@zaza", "zaza19@mail.ru"),
+       ("Анна", "Леонова", "Максимовна", "github.com/cadacko", "+71112940425", "@cadacko", "cadacko@gmail.com"),
+       ("Варвара", "Ефремова", "Елисеевна", "github.com/wzinasttr", "+78990657193", "@wzinasttr", "wzinasttr@mail.ru"),
+       ("Амина", "Андреева", "Серафимовна", "github.com/yona", "89881692223", "@yona", "yona1676@yandex.ru"),
+       ("Никита", "Петров", "Андреевич", null, null, null, null),
+       ("Арина", "Киселева", "Ивановна", "github.com/cinta", "82536871161", "@cinta", "cinta@gmail.com"),
+       ("Дарья", "Семенова", "Вадимовна", "github.com/usthangha", "+76844732604", "@usthangha", "usthangha_super@gmail.com"),
+       ("Дмитрий", "Рогов", "Романович", "github.com/avin", "+71650056412", "@avin", "avinavin@mail.ru"),
+       ("Платон", "Новиков", "Артемьевич", "github.com/haranars", "+78344631475", null, "haranars1@yandex.ru"),
+       ("Дмитрий", "Ковалев", "Даниилович", "github.com/ugir", "+72299982158", "@ugir", "uuugir@yandex.ru");
---
+++ StudentProject/database/scripts/structure.sql
@@ -0,0 +1,10 @@
+create table if not exists Students (
+    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+	Name varchar(20) not null,
+	Family	varchar(20) not null,
+	Patronymic varchar(20) not null,
+	Git	varchar(50) null,
+	PhoneNumber varchar(12) null,
+	Telegram varchar(30) null,
+	Email varchar(50) null
+);

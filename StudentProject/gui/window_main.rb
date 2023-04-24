880be19ca8c3ce2132feabbb94db9b4934d722f5
KirSaxhlil <dimapisarenko28@gmail.com>
2023-04-24 07:14:13 +0300

    Лаб.3 Здч.3 Добавлена таблица

--- StudentProject/gui/window_main.rb
+++ StudentProject/gui/window_main.rb
@@ -32,6 +32,22 @@ class WindowMain < FXMainWindow
         bf_phone = FXBlockFilter.new(column_filter, "Есть телефон?")
         bf_telegram = FXBlockFilter.new(column_filter, "Есть телеграм?")
 
+        column_table = FXVerticalFrame.new(frame, opts:LAYOUT_FILL)
+        table = FXTableStudents.new(column_table)
+        table.appendRows(4) #test
+        table.setItemText(0, 0, "213") #test
+        table.setItemText(1, 0, "343") #test
+        table.setItemText(2, 0, "124") #test
+        table.setItemText(3, 0, "5") #test
+        undertable_horizon = FXHorizontalFrame.new(column_table, opts:LAYOUT_FILL_X)
+        undertable_horizon_left = FXHorizontalFrame.new(undertable_horizon, opts:LAYOUT_LEFT)
+        FXLabel.new(undertable_horizon_left, "Элементов на странице:")
+        count_input = FXTextField.new(undertable_horizon_left, 10, opts:FRAME_SUNKEN|FRAME_THICK|LAYOUT_FIX_WIDTH, width:30)
+        undertable_horizon_rigth = FXHorizontalFrame.new(undertable_horizon, opts:LAYOUT_RIGHT)
+        btn_prev_page = FXButton.new(undertable_horizon_rigth, "<<")
+        FXLabel.new(undertable_horizon_rigth, "Страница X из N")
+        btn_next_page = FXButton.new(undertable_horizon_rigth, ">>")
+
         
         
     end
@@ -59,3 +75,44 @@ class FXBlockFilter
     end
 end
 
+class FXTableStudents < FXTable
+    def initialize(parent)
+        super(parent, width:300, opts:TABLE_READONLY|TABLE_COL_SIZABLE|TABLE_ROW_RENUMBER|LAYOUT_FILL|FRAME_SUNKEN|FRAME_THICK)
+        self.appendColumns(4)
+
+        self.setColumnText(0, "ID")
+        self.setColumnText(1, "ФИО")
+        self.setColumnText(2, "Гит")
+        self.setColumnText(3, "Контакт")
+
+        self.setColumnWidth(0, 50)
+        self.setColumnWidth(1, 150)
+        self.setColumnWidth(2, 200)
+        self.setColumnWidth(3, 100)
+
+        self.setRowHeaderWidth(30)
+
+        self.getColumnHeader.connect(SEL_COMMAND) do |a, b, column|
+            sort_by_column(column)
+          end
+    end
+
+    def sort_by_column(column)
+        #ере
+        table_data = (0...self.getNumRows()).map { |row_index| (0...self.getNumColumns()).map { |col_index| self.getItemText(row_index, col_index) } }
+    
+        sorted_table_data = table_data.sort_by { |row_data|
+            if row_data[column] =~ /^\d+$/
+                [2, $&.to_i]
+            else
+                [1, row_data[column]]
+            end
+        }
+    
+        sorted_table_data.each_with_index do |row_data, row_index|
+          row_data.each_with_index do |cell_data, col_index|
+            self.setItemText(row_index, col_index, cell_data)
+          end
+        end
+      end
+end
